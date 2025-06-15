7bbe6b69ab79d3eb92cc515ca67a059f07837c5b
Breno Pavanelli dos Santos <zayocomercial@gmail.com>
2025-06-14 23:32:26 -0300

    fix: svg check centralizado
    remoção de regras antigas complexas para animação do svg e atualização do modal dentro da função showModal visando a compatibilidade com Flexbox

--- js/script.js
+++ js/script.js
@@ -81,9 +81,8 @@ function showModal(type, messages = []) {
     if (type === 'success') {
         modalContentHTML = `
             <div class="modal-content">
-                <div class="success-animation">
-                    <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
-                        <circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/>
+                <div class="success-icon">
+                    <svg class="checkmark-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
                         <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
                     </svg>
                 </div>
